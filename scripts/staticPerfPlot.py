6b6a59fbaaf1e457e387128dd4618c056b3ae4c0
adam <adam.lange@langeturbo.com>
2015-12-21 21:36:14 -0600

    improvements to staticPerfRun and plot

--- scripts/staticPerfPlot.py
+++ scripts/staticPerfPlot.py
@@ -3,23 +3,24 @@ import matplotlib.pyplot as plt
 import os
 import re
 
+def assembleDf(directoryPath):
+  #find all the postprocessor outputfiles and
+  # and read each in to a dataframe
+  p = re.compile(r'postprocessor_(.*)\.csv')
+  #p = re.compile(r'output_[0-9]*\.log')
+  dfs = []
+  for directory,directories,files in os.walk(directoryPath):
+    for f in files:
+      m = p.match(f)
+      if m:
+        dfd = pd.read_csv(directory+'/'+f)
+        num = m.expand(r'\1')
+        dfi = pd.read_csv(directory+'/indep_{}.csv'.format(num))
+        for col in dfi.columns:
+          dfd[col] = dfi[col][0]
 
-#find all the postprocessor outputfiles and
-# and read each in to a dataframe
-p = re.compile(r'postprocessor_([0-9])*\.csv')
-#p = re.compile(r'output_[0-9]*\.log')
-dfs = []
-for directory,directories,files in os.walk('.'):
-  for f in files:
-    m = p.match(f)
-    if m:
-      dfd = pd.read_csv(directory+'/'+f)
-      num = m.expand(r'\1')
-      dfi = pd.read_csv(directory+'/indep_{}.csv'.format(num))
-      for col in dfi.columns:
-        dfd[col] = dfi[col][0]
+        dfs.append(dfd)
 
-      dfs.append(dfd)
-
-df = pd.concat(dfs)
-df = df[df.index==0]
+  df = pd.concat(dfs)
+  df = df[df.index==0]
+  return df
--- scripts/staticPerfRun.py
+++ scripts/staticPerfRun.py
@@ -2,10 +2,16 @@ import subprocess
 import numpy as np
 import os
 import pandas as pd
+from time import localtime
+from hashlib import md5
 
 # input file
 inputFile = 'ex01.i'
 numProc = 6
+
+# mesh files
+#mesh_array = [('filename',float integrated_area, floathole_area)]
+mesh_array = [('mesh1',-1,-1)]
 
 # control for h, the film thickness
 min_h = 10e-6 #m
@@ -48,47 +54,54 @@ alpha_array = np.linspace(min_alpha,max_alpha,n_alpha)
 
 commands = []
 
-for h in h_array:
-  for ps in ps_array:
-    for pamb in pamb_array:
-      for T in T_array:
-        for v in v_array:
-          for alpha in alpha_array:
-            cmd_str = (
-                'mpiexec -n {} slug-opt -i {} '.format(numProc,inputFile)
-                +'AuxKernels/h/h={} '.format(h)
-                +'BCs/inlet/value={} '.format(ps)
-                +'BCs/perimeter/value={} '.format(pamb)
-                +'GlobalParams/vel_surface=\'{} 0 0\' '.format(v)
-                +'Kernels/Pressure/v={} '.format(v)
-                +'AuxKernels/h/alpha={} '.format(alpha)
-                +'--show-input '
-                +'--no-color '
-                )
-            df = pd.DataFrame({
-                'h':[h],
-                'ps':[ps],
-                'pamb':[pamb],
-                'T':[T],
-                'v':[v],
-                'alpha':[alpha]
-              })
-            commands.append([cmd_str,df])
+for mesh in mesh_array:
+  for h in h_array:
+    for ps in ps_array:
+      for pamb in pamb_array:
+        for T in T_array:
+          for v in v_array:
+            for alpha in alpha_array:
+              cmd_str = (
+                  'mpiexec -n {} slug-opt -i {} '.format(numProc,inputFile)
+                  +'Mesh/file={} '.format(mesh[0])
+                  +'AuxKernels/h/h={} '.format(h)
+                  +'BCs/inlet/value={} '.format(ps)
+                  +'BCs/perimeter/value={} '.format(pamb)
+                  +'GlobalParams/vel_surface=\'{} 0 0\' '.format(v)
+                  +'Kernels/Pressure/v={} '.format(v)
+                  +'AuxKernels/h/alpha={} '.format(alpha)
+                  +'--show-input '
+                  +'--no-color '
+                  )
+              df = pd.DataFrame({
+                  'hash':[md5(str(localtime())+str(h)+str(ps)+str(pamb)+str(T)+str(v)+str(alpha)).hexdigest()],
+                  'mesh':[mesh[0]],
+                  'film_area':[mesh[1]],
+                  'inlet_area':[mesh[2]],
+                  'h':[h],
+                  'ps':[ps],
+                  'pamb':[pamb],
+                  'T':[T],
+                  'v':[v],
+                  'alpha':[alpha]
+                })
+              commands.append([cmd_str,df])
 
 # divy out the jobs (each job is a mpi job, and multiple 
 # mpi jobs can go at once.)
 
 num_commands = len(commands)
 for i in range(num_commands):
+  hsh = commands[i][1]['hash'].iloc[0]
   cmd = commands[i][0]\
-    +'Outputs/exodus/file_base=exodus_{} '.format(i)\
-    +'Outputs/csv/file_base=postprocessor_{}'.format(i)\
-    +'>&1 > output_{}.log'.format(i)
+    +'Outputs/exodus/file_base=exodus_{} '.format(hsh)\
+    +'Outputs/csv/file_base=postprocessor_{}'.format(hsh)\
+    +'>&1 > output_{}.log'.format(hsh)
 
   print(
-    '\nBeginning solve {} of {}\n'.format((i+1),num_commands)
+    '\nBeginning solve {} of {}\nhash: {}\n'.format((i+1),num_commands,hsh)
     +cmd
   )
-  commands[i][1].to_csv('./indep_{}.csv'.format(i))
+  commands[i][1].to_csv('./indep_{}.csv'.format(hsh))
   os.system(cmd)
 
